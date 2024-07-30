24cac3666120412d39e41028f48cda4148658c55
Dat Hoang <fiezt@outlook.com>
2024-07-30 10:24:34 +0700

    style: format with 'bun format'

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,62 +1,62 @@
-import '@/styles/global.css';
+import '@/styles/global.css'
 
-import type { Metadata } from 'next';
-import { NextIntlClientProvider, useMessages } from 'next-intl';
-import { unstable_setRequestLocale } from 'next-intl/server';
+import type { Metadata } from 'next'
+import { NextIntlClientProvider, useMessages } from 'next-intl'
+import { unstable_setRequestLocale } from 'next-intl/server'
 
-import { DemoBadge } from '@/components/DemoBadge';
-import { AppConfig } from '@/utils/AppConfig';
+import { DemoBadge } from '@/components/DemoBadge'
+import { AppConfig } from '@/utils/AppConfig'
 
 export const metadata: Metadata = {
-  icons: [
-    {
-      rel: 'apple-touch-icon',
-      url: '/apple-touch-icon.png',
-    },
-    {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '32x32',
-      url: '/favicon-32x32.png',
-    },
-    {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '16x16',
-      url: '/favicon-16x16.png',
-    },
-    {
-      rel: 'icon',
-      url: '/favicon.ico',
-    },
-  ],
-};
+    icons: [
+        {
+            rel: 'apple-touch-icon',
+            url: '/apple-touch-icon.png',
+        },
+        {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '32x32',
+            url: '/favicon-32x32.png',
+        },
+        {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '16x16',
+            url: '/favicon-16x16.png',
+        },
+        {
+            rel: 'icon',
+            url: '/favicon.ico',
+        },
+    ],
+}
 
 export function generateStaticParams() {
-  return AppConfig.locales.map((locale) => ({ locale }));
+    return AppConfig.locales.map((locale) => ({ locale }))
 }
 
 export default function RootLayout(props: {
-  children: React.ReactNode;
-  params: { locale: string };
+    children: React.ReactNode
+    params: { locale: string }
 }) {
-  unstable_setRequestLocale(props.params.locale);
+    unstable_setRequestLocale(props.params.locale)
 
-  // Using internationalization in Client Components
-  const messages = useMessages();
+    // Using internationalization in Client Components
+    const messages = useMessages()
 
-  return (
-    <html lang={props.params.locale}>
-      <body>
-        <NextIntlClientProvider
-          locale={props.params.locale}
-          messages={messages}
-        >
-          {props.children}
+    return (
+        <html lang={props.params.locale}>
+            <body>
+                <NextIntlClientProvider
+                    locale={props.params.locale}
+                    messages={messages}
+                >
+                    {props.children}
 
-          <DemoBadge />
-        </NextIntlClientProvider>
-      </body>
-    </html>
-  );
+                    <DemoBadge />
+                </NextIntlClientProvider>
+            </body>
+        </html>
+    )
 }
--- src/components/GuestbookList.tsx
+++ src/components/GuestbookList.tsx
@@ -1,33 +1,33 @@
-import { db } from '@/libs/DB';
-import { logger } from '@/libs/Logger';
-import { guestbookSchema } from '@/models/Schema';
+import { db } from '@/libs/DB'
+import { logger } from '@/libs/Logger'
+import { guestbookSchema } from '@/models/Schema'
 
-import { DeleteGuestbookEntry } from './DeleteGuestbookEntry';
-import { EditableGuestbookEntry } from './EditableGuestbookEntry';
+import { DeleteGuestbookEntry } from './DeleteGuestbookEntry'
+import { EditableGuestbookEntry } from './EditableGuestbookEntry'
 
 const GuestbookList = async () => {
-  const guestbook = await db
-    .select()
-    .from(guestbookSchema)
-    .orderBy(guestbookSchema.createdAt);
+    const guestbook = await db
+        .select()
+        .from(guestbookSchema)
+        .orderBy(guestbookSchema.createdAt)
 
-  logger.info('Get all guestbook entries');
+    logger.info('Get all guestbook entries')
 
-  return (
-    <div className="mt-5" data-testid="guestbook-list">
-      {guestbook.map((elt) => (
-        <div key={elt.id} className="mb-1 flex items-center gap-x-1">
-          <DeleteGuestbookEntry id={elt.id} />
+    return (
+        <div className="mt-5" data-testid="guestbook-list">
+            {guestbook.map((elt) => (
+                <div key={elt.id} className="mb-1 flex items-center gap-x-1">
+                    <DeleteGuestbookEntry id={elt.id} />
 
-          <EditableGuestbookEntry
-            id={elt.id}
-            username={elt.username}
-            body={elt.body}
-          />
+                    <EditableGuestbookEntry
+                        id={elt.id}
+                        username={elt.username}
+                        body={elt.body}
+                    />
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
-};
+    )
+}
 
-export { GuestbookList };
+export { GuestbookList }
